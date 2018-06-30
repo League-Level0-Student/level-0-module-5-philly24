4ec123fef7ded8fab76b517327edb1121f222cca
Phillip <Phillip@yens-mbp>
2018-06-29 23:06:15 -0700

    code4life

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,18 +7,30 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class Code4Life {
+	public static void main(String[] args) {
+		
 	
 	//1.  Ask the user how many hours they spent coding this week.
-	
+	String answer = JOptionPane.showInputDialog("how many hours did you spend coding this week?");
+	int answer2 = Integer.parseInt(answer);
 	//2.  If it is less than or equal to 2, tell them to stop watching YouTube and write code instead.
-	
+	if(answer2<=2) {
+		JOptionPane.showMessageDialog(null, "stop watching youtube and write code instead");
+	}
 	//3.  If it is greater than or equal to 3 AND less than or equal to 5, tell them they're a Code Ninja
-	
+	else if (answer2>=3) {
+		JOptionPane.showMessageDialog(null, "you are a code ninja");
+	}
 	//4.  If it is more than 5, call the method below to play the Batman theme song.  
-
+	else if (answer2>5) {
+		Code4Life.playBatmanTheme();
+	}
+	
+	}
     private static void playBatmanTheme() {
     	try {
    		 Clip clip = AudioSystem.getClip();
@@ -32,3 +44,4 @@ public class Code4Life {
 
 
 }
+	
